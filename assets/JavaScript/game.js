180592c6c0ee495c4d28d7a57c2c6274b1ffcbb0
gpelleran78 <geremy_pelleran@yahoo.com>
2019-09-05 10:09:37 -0700

    added random word generator

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,14 +1,39 @@
-alert('You have 10 guesses. Guess 1-10');
+// alert('You have 10 guesses. Guess 1-10');
 
-var answer = 3;
-var guess = prompt("Whats your guess?");
+// var answer = 3;
+// var guess = prompt("Whats your guess?");
 
-for (i = 0; i < 10; i++) {
-    if (answer == guess) {
-        alert("You guessed correctly");
-        break;
+// for (i = 0; i < 10; i++) {
+//     if (answer == guess) {
+//         alert("You guessed correctly");
+//         break;
+//     }
+//     else {
+//         guess = prompt("Please Try Again");
+//     }
+// };
+//========Global============
+var wordBank = ['Joker','Harley-Quinn','BatMan','Wonder-Woman',"Flash"];
+var wins = 0;
+var losses = 0;
+var wrongLetter = [];
+var guessesLeft = 10;
+var underScore = [];
+var randWord;
+
+//======Function==========
+function startGame(){
+
+    //random word generator
+    randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    
+    for (var i = 0; i < randWord.length; i++);
+    {
+
     }
-    else {
-        guess = prompt("Please Try Again");
-    }
-};
+    console.log(randWord);
+   
+}
+
+
+startGame();
